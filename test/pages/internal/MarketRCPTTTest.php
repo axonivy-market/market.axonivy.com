2004a893e3eeea3ea66bc4e260f738d38c1b3448
Alex <alexander.suter@axonivy.com>
2022-10-24 10:46:10 +0200

    Revert "Revert "Rewrite backend""

--- test/pages/internal/MarketRCPTTTest.php
+++ test/pages/internal/MarketRCPTTTest.php
@@ -9,10 +9,7 @@ class MarketRCPTTTest extends TestCase
 {
   public function testRender()
   {
-    $_SERVER['HTTPS'] = 'https';
-    $_SERVER['HTTP_HOST'] = 'fakehost';
-    
-    AppTester::assertThatGet('/internal/market-rcptt?designerVersion=9.2.0')
+    AppTester::assertThatGet('https://fakehost/internal/market-rcptt?designerVersion=9.2.0')
       ->ok()
       ->bodyContains('runTest "https://fakehost/market-cache/error-handling')
       ->bodyDoesNotContain('https://fakehost/market-cache/a-trust/a-trust-connector-product');
